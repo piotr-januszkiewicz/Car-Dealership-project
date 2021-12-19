16e6cfebdd827a776a980ba47af8e68ef1d56595
Piotr Januszkiewicz <30535818+piotrowski128@users.noreply.github.com>
2021-12-19 21:18:45 +0100

    Delete CarsDealership directory

--- CarsDealership/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module pl.polsl.carsdealership {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens pl.polsl.carsdealership to javafx.fxml;
-    exports pl.polsl.carsdealership;
-}
